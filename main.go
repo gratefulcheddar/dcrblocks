c7e185849c74a61c524e4019c53a0e06ae00b24a
gratefulcheddar <timthomascode@icloud.com>
2017-09-22 21:37:35 -0500

    Finish basic functionality to handle block URLs

--- main.go
+++ main.go
@@ -12,28 +12,14 @@ import (
 	"net/http"
 	"path/filepath"
 	"strconv"
-	"time"
 
-	"github.com/decred/dcrd/dcrjson"
 	"github.com/decred/dcrrpcclient"
 	"github.com/decred/dcrutil"
 )
 
-func blockHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("block.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	type Block struct {
-		Height string
-		Hash   string
-		Size   string
-	}
-	tBlock := Block{"420", "Yes", "Phat"}
-	t.Execute(w, tBlock)
-}
+func handleBlock(w http.ResponseWriter, r *http.Request, client *dcrrpcclient.Client) {
 
-func getBlock(client *dcrrpcclient.Client, blockStr string) *dcrjson.GetBlockVerboseResult {
+	blockStr := r.URL.Path[7:]
 
 	blockInt64, err := strconv.ParseInt(blockStr, 10, 64)
 	if err != nil {
@@ -48,75 +34,46 @@ func getBlock(client *dcrrpcclient.Client, blockStr string) *dcrjson.GetBlockVer
 		log.Fatal(err)
 	}
 
-	return block
+	t, err := template.ParseFiles("block.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	t.Execute(w, block)
+
+	client.Shutdown()
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, *dcrrpcclient.Client)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		// Connect to local dcrd RPC server using websockets.
+		dcrdHomeDir := dcrutil.AppDataDir("dcrd", false)
+		certs, err := ioutil.ReadFile(filepath.Join(dcrdHomeDir, "rpc.cert"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		connCfg := &dcrrpcclient.ConnConfig{
+			Host:         "localhost:9109",
+			Endpoint:     "ws",
+			User:         "timthomas",
+			Pass:         "CfG/BcB1M7q5haQtc6kit34mJycT+bOI",
+			Certificates: certs,
+		}
+		client, err := dcrrpcclient.New(connCfg, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fn(w, r, client)
+	}
 }
 
 func main() {
-	// Only override the handlers for notifications you care about.
-	// Also note most of these handlers will only be called if you register
-	// for notifications.  See the documentation of the dcrrpcclient
-	// NotificationHandlers type for more details about each handler.
-	ntfnHandlers := dcrrpcclient.NotificationHandlers{
-		OnBlockConnected: func(blockHeader []byte, transactions [][]byte) {
-			log.Printf("Block connected: %v %v", blockHeader, transactions)
-		},
-		OnBlockDisconnected: func(blockHeader []byte) {
-			log.Printf("Block disconnected: %v", blockHeader)
-		},
-	}
 
-	// Connect to local dcrd RPC server using websockets.
-	dcrdHomeDir := dcrutil.AppDataDir("dcrd", false)
-	certs, err := ioutil.ReadFile(filepath.Join(dcrdHomeDir, "rpc.cert"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	connCfg := &dcrrpcclient.ConnConfig{
-		Host:         "localhost:9109",
-		Endpoint:     "ws",
-		User:         "timthomas",
-		Pass:         "CfG/BcB1M7q5haQtc6kit34mJycT+bOI",
-		Certificates: certs,
-	}
-	client, err := dcrrpcclient.New(connCfg, &ntfnHandlers)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Register for block connect and disconnect notifications.
-	if err := client.NotifyBlocks(); err != nil {
-		log.Fatal(err)
-	}
-	log.Println("NotifyBlocks: Registration Complete")
-
-	// Get the current block count.
-	blockCount, err := client.GetBlockCount()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Block count: %d", blockCount)
-
-	getBlock(client, "80193")
-
-	// Start an http server @ localhost:8080 and handle
-	// the /block/ URL
 	server := http.Server{
 		Addr: "localhost:8080",
 	}
-	http.HandleFunc("/block/", blockHandler)
+	http.HandleFunc("/block/", makeHandler(handleBlock))
+	log.Printf("Server started. Address = %v", server.Addr)
 	server.ListenAndServe()
 
-	// Wait until the client either shuts down gracefully (or the user
-	// terminates the process with Ctrl+C).
-	client.WaitForShutdown()
-
-	// For this example gracefully shutdown the client after 10 seconds.
-	// Ordinarily when to shutdown the client is highly application
-	// specific.
-	log.Println("Client shutdown in 2 seconds...")
-	time.AfterFunc(time.Second*2, func() {
-		log.Println("Client shutting down...")
-		client.Shutdown()
-		log.Println("Client shutdown complete.")
-	})
 }
