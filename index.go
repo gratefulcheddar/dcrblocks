282d4a1ce3e772ea77c71cd4211bf39eed603a60
gratefulcheddar <timthomascode@icloud.com>
2017-12-01 22:46:29 -0600

    Finish index prototype

--- index.go
+++ index.go
@@ -3,19 +3,22 @@ package main
 import (
 	"html/template"
 	"log"
+	"math"
 	"net/http"
-
-	"github.com/decred/dcrd/dcrjson"
+	"time"
 
 	"github.com/decred/dcrd/rpcclient"
 )
 
 type indexBlocks struct {
-	VerboseBlock *dcrjson.GetBlockVerboseResult
-	Age          int
-	TxnCount     int
-	VoteCount    int
-	TicketCount  int
+	Height      int64
+	Size        int32
+	Age         float64
+	TxnCount    int
+	VoteCount   int
+	TicketCount int
+	RevokeCount int
+	Reward      float64
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request, client *rpcclient.Client) {
@@ -39,9 +42,36 @@ func handleIndex(w http.ResponseWriter, r *http.Request, client *rpcclient.Clien
 			log.Fatal(err)
 		}
 
-		topTenBlocks[i].VerboseBlock, err = client.GetBlockVerbose(blockHash, true)
+		verboseBlock, err := client.GetBlockVerbose(blockHash, true)
 		if err != nil {
 			log.Fatal(err)
+		}
+
+		topTenBlocks[i].Height = verboseBlock.Height
+		topTenBlocks[i].Size = verboseBlock.Size
+
+		topTenBlocks[i].Age = math.Floor(time.Now().Sub(time.Unix(verboseBlock.Time, 0)).Minutes())
+
+		topTenBlocks[i].TxnCount = len(verboseBlock.RawTx)
+
+		topTenBlocks[i].VoteCount = 0
+		topTenBlocks[i].TicketCount = 0
+		topTenBlocks[i].RevokeCount = 0
+
+		for j := 0; j < len(verboseBlock.RawSTx); j++ {
+			if verboseBlock.RawSTx[j].Vout[0].ScriptPubKey.Type == "stakesubmission" {
+				topTenBlocks[i].TicketCount++
+			} else if verboseBlock.RawSTx[j].Vout[0].ScriptPubKey.Type == "stakerevoke" {
+				topTenBlocks[i].RevokeCount++
+			} else if verboseBlock.RawSTx[j].Vout[2].ScriptPubKey.Type == "stakegen" {
+				topTenBlocks[i].VoteCount++
+			}
+		}
+
+		for h := 0; h < len(verboseBlock.RawTx); h++ {
+			if verboseBlock.RawTx[h].Vin[0].IsCoinBase() {
+				topTenBlocks[i].Reward = verboseBlock.RawTx[h].Vin[0].AmountIn
+			}
 		}
 
 	}
